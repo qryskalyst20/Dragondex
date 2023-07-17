72e590fe1836362cf810d64f52543ac89da15eb3
Qagura <quddusnuriman@gmail.com>
2023-07-17 21:42:37 +0800

    replaced puppeteer with cheerios and axios for web scraping

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -1,34 +1,34 @@
-// let puppeteer = require("puppeteer");
-// let fs = require("fs");
-// import { AsyncStorage } from "react-native";
+// // let puppeteer = require("puppeteer");
+// // let fs = require("fs");
+// // import { AsyncStorage } from "react-native";
 
-const url = "https://dragoncity.fandom.com/wiki/Dragons/All";
+// const url = "https://dragoncity.fandom.com/wiki/Dragons/All";
 
-const scraper = async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(url);
-  // await page.screenshot({ path: "josh-comeau.png" });
-  const allEggs = await page.evaluate(() => {
-    const eggs = document.querySelectorAll(".bm_dragon_name");
+// const scraper = async () => {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+//   await page.goto(url);
+//   // await page.screenshot({ path: "josh-comeau.png" });
+//   const allEggs = await page.evaluate(() => {
+//     const eggs = document.querySelectorAll(".bm_dragon_name");
 
-    return Array.from(eggs)
-      .slice(0, 10)
-      .map((egg) => {
-        const dragonName = egg.querySelector("span").innerText;
-        const url = egg.querySelector("a").href;
-        const imageUrl = egg.querySelector("img").src;
-        return { dragonName, url, imageUrl };
-      });
-  });
+//     return Array.from(eggs)
+//       .slice(0, 10)
+//       .map((egg) => {
+//         const dragonName = egg.querySelector("span").innerText;
+//         const url = egg.querySelector("a").href;
+//         const imageUrl = egg.querySelector("img").src;
+//         return { dragonName, url, imageUrl };
+//       });
+//   });
 
-  // console.log(allEggs);
-  // const fs = require("fs");
+//   // console.log(allEggs);
+//   // const fs = require("fs");
 
-  //WRITE TO FILE
-  // fs.writeFile("./dragons.json", JSON.stringify(allEggs), (err) =>
-  //   err ? console.log(err) : null
-  // );
-};
+//   //WRITE TO FILE
+//   // fs.writeFile("./dragons.json", JSON.stringify(allEggs), (err) =>
+//   //   err ? console.log(err) : null
+//   // );
+// };
 
-scraper();
+// scraper();
--- screens/DragonsScreen.js
+++ screens/DragonsScreen.js
@@ -12,7 +12,10 @@ import React, { useCallback, useEffect, useState } from "react";
 import * as SplashScreen from "expo-splash-screen";
 import { useFonts } from "expo-font";
 import { Icon } from "@rneui/base";
+// import "expect-puppeteer";
 // import puppeteer from "puppeteer";
+import axios from "axios";
+import cheerio from "cheerio";
 import dragonData from "../scraper/dragons.json";
 // import scraper from "../scraper/scraper.js";
 
@@ -27,16 +30,40 @@ export default function DragonsScreen({ navigation }) {
     }, 2000);
   }, []);
 
-  const [dragonNames, setDragonNames] = useState([]);
-  useEffect(() => {
-    const names = dragonData.map((dragon) => ({
-      dragonName: dragon.dragonName,
-      imageUrl: dragon.imageUrl,
-    }));
-    setDragonNames(names);
-  }, []);
+  // const [dragonNames, setDragonNames] = useState([]);
+  // useEffect(() => {
+  //   const names = dragonData.map((dragon) => ({
+  //     dragonName: dragon.dragonName,
+  //     imageUrl: dragon.imageUrl,
+  //   }));
+  //   setDragonNames(names);
+  // }, []);
 
   // const [dragonData, setDragonData] = useState([]);
+  // const url = "https://dragoncity.fandom.com/wiki/Dragons/All";
+
+  // const scraper = async () => {
+  //   const browser = await puppeteer.launch();
+  //   const page = await browser.newPage();
+  //   await page.goto(url);
+  //   const allEggs = await page.evaluate(() => {
+  //     const eggs = document.querySelectorAll(".bm_dragon_name");
+
+  //     return Array.from(eggs)
+  //       .slice(0, 10)
+  //       .map((egg) => {
+  //         const dragonName = egg.querySelector("span").innerText;
+  //         const url = egg.querySelector("a").href;
+  //         const imageUrl = egg.querySelector("img").src;
+  //         return { dragonName, url, imageUrl };
+  //       });
+  //   });
+  //   setDragonData(allEggs);
+  // };
+
+  // useEffect(() => {
+  //   scraper();
+  // }, []);
 
   // useEffect(() => {
   //   const fetchData = async () => {
@@ -46,6 +73,31 @@ export default function DragonsScreen({ navigation }) {
 
   //   fetchData();
   // }, []);
+
+  const scraper = async () => {
+    const response = await axios.get(
+      "https://dragoncity.fandom.com/wiki/Dragons/All"
+    );
+    const html = response.data;
+
+    const $ = cheerio.load(html);
+    // Select all the elements with the class name "athing"
+    const articles = $(".bm_dragon_name");
+
+    // Loop through the selected elements
+    for (const article of articles) {
+      // Organize the extracted data in an object
+      const structuredData = {
+        imageUrl: decodeURIComponent(
+          $(article).find(".bm_dragon_square a img").attr("data-src")
+        ),
+        dragonName: $(article).find("span").text(),
+      };
+
+      // Log each element's strcutured data results to the console
+      console.log(structuredData);
+    }
+  };
 
   const [fontsLoaded] = useFonts({
     "SF-Bold": require("../assets/fonts/SF-Pro-Text-Bold.otf"),
@@ -84,7 +136,8 @@ export default function DragonsScreen({ navigation }) {
       > */}
       <TouchableOpacity
         className="mt-[30px] ml-5 self-start"
-        onPress={() => navigation.navigate("homescreen")}
+        // onPress={() => navigation.navigate("homescreen")}
+        onPress={scraper}
       >
         <Icon type="antdesign" name="arrowleft" color={"#fff"} size={30} />
       </TouchableOpacity>
@@ -94,7 +147,7 @@ export default function DragonsScreen({ navigation }) {
           className="text-white text-4xl ml-5"
           style={{ fontFamily: "SF-Bold" }}
         >
-          Dragons
+          Dragonss
         </Text>
         <View className="w-[90%] mx-auto flex-row rounded-full mt-5 border-[1px] py-4 px-5 bg-[#121212] text-slate-100">
           <Icon
@@ -116,8 +169,8 @@ export default function DragonsScreen({ navigation }) {
       </View>
 
       <View className="w-screen flex-1">
-        <FlatList
-          data={dragonNames}
+        {/* <FlatList
+          data={dragonData}
           keyExtractor={(item, index) => index.toString()}
           numColumns={2}
           columnWrapperStyle={{ justifyContent: "space-between" }}
@@ -132,7 +185,7 @@ export default function DragonsScreen({ navigation }) {
               />
             </View>
           )}
-        />
+        /> */}
       </View>
     </SafeAreaView>
   );
